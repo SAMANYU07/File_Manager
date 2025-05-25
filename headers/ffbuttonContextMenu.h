3063e592a2887c487c147dda4fb61488c9416768
SAMANYU07 <samanyu1003@gmail.com>
2025-05-25 11:25:23 +0530

    Fixed rContextMenu not clickable

--- headers/ffbuttonContextMenu.h
+++ headers/ffbuttonContextMenu.h
@@ -1,48 +1,55 @@
 #include <iostream>
 #include <gtk-3.0/gtk/gtk.h>
 
-GtkWidget *popover;
 
-class FfButtonPopupMenu
-{
-        public:
-        static FfButtonPopupMenu *instance;
-        static void deletePopover()
-        {
-                std::cout << "delete_popover\n";
-                if (instance != nullptr)
-                {
-                        delete instance;
-                        instance = nullptr;
-                }
+GtkWidget *popover = nullptr;
+
+class FfButtonPopupMenu {
+public:
+    static FfButtonPopupMenu *instance;
+
+    static void deletePopover(GtkWidget *widget, gpointer data) {
+        std::cout << "delete_popover\n";
+        if (instance != nullptr) {
+            gtk_widget_destroy(popover);  // Properly destroy the widget
+            delete instance;
+            instance = nullptr;
         }
-        FfButtonPopupMenu(GtkWidget *widget, GdkEventButton *event, gpointer data)
-        {
-                popover = gtk_popover_new(widget);
-                GtkWidget *popover_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-                GtkWidget *test = gtk_label_new("text_label");
-                gtk_container_add(GTK_CONTAINER(popover), popover_box);
-                gtk_box_pack_start(GTK_BOX(popover_box), test, FALSE, FALSE, 0);
-                gtk_widget_show_all(popover);
-                // gtk_popover_popdown(GTK_POPOVER(popover));
-                // gtk_popover_set_modal(GTK_POPOVER(popover), TRUE);
-                gtk_widget_grab_focus(popover);
-                g_signal_connect(popover, "focus-out-event", G_CALLBACK(deletePopover), nullptr);
-        }
-        static gboolean initFfButtonPopupMenu(GtkWidget *widget, GdkEventButton *event, gpointer data)
-        {
-                if (event->button == GDK_BUTTON_PRIMARY)
-                        return FALSE;
-                // if (instance == nullptr)
-                        instance = new FfButtonPopupMenu(widget, event, data);
-                        return TRUE;
-                // else
-                // {
-                //         delete instance;
-                //         instance = nullptr;
-                // }
-                // return instance;
-        }
+    }
+
+    FfButtonPopupMenu(GtkWidget *widget, GdkEventButton *event) {
+        popover = gtk_popover_new(widget);
+        gtk_popover_set_position(GTK_POPOVER(popover), GTK_POS_BOTTOM);
+        gtk_popover_set_modal(GTK_POPOVER(popover), TRUE); // Optional, helps keep it open
+
+        GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+        GtkWidget *label = gtk_label_new("Options:");
+        gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 0);
+
+        // Optional: add a button to close the popover
+        GtkWidget *close_btn = gtk_button_new_with_label("Close");
+        GtkWidget *copyButton = gtk_button_new_with_label("Copy");
+        gtk_box_pack_start(GTK_BOX(box), close_btn, FALSE, FALSE, 0);
+        gtk_box_pack_start(GTK_BOX(box), copyButton, false, false, 0);
+        g_signal_connect(close_btn, "clicked", G_CALLBACK(deletePopover), nullptr);
+        g_signal_connect(copyButton, "clicked", G_CALLBACK(deletePopover), nullptr);
+
+        gtk_container_add(GTK_CONTAINER(popover), box);
+        gtk_widget_show_all(popover);
+
+        gtk_popover_popup(GTK_POPOVER(popover));
+    }
+
+    static gboolean initFfButtonPopupMenu(GtkWidget *widget, GdkEventButton *event, gpointer data) {
+        if (event->button == GDK_BUTTON_PRIMARY)
+            return FALSE;
+
+        if (instance != nullptr)
+            deletePopover(nullptr, nullptr);
+
+        instance = new FfButtonPopupMenu(widget, event);
+        return TRUE;
+    }
 };
 
 FfButtonPopupMenu *FfButtonPopupMenu::instance = nullptr;
--- main.cpp
+++ main.cpp
@@ -413,7 +413,7 @@ void ffpaneInsert(std::string ffList)
                 // g_signal_connect(ffbutton, "leave-notify-event", G_CALLBACK(ggh::on_leaving_ffButton), g_strdup(truncatedLabel.c_str()));
                 gtk_widget_set_name(ffbutton, "ffbutton");
                 // g_signal_connect(ffbutton, "button-press-event", G_CALLBACK(Popup::getInstance), ffbutton);
-                g_signal_connect(ffbutton, "button-press-event", G_CALLBACK(FfButtonPopupMenu::initFfButtonPopupMenu), NULL);
+                g_signal_connect(ffbutton, "button-press-event", G_CALLBACK(FfButtonPopupMenu::initFfButtonPopupMenu), ffbutton);
                 g_signal_connect(ffbutton, "clicked", G_CALLBACK(ffClickedF), ffbutton);
                 g_signal_connect(ffbutton, "pressed", G_CALLBACK(ffPressedF), ffbutton);
                 g_signal_connect(ffbutton, "clicked", G_CALLBACK(ffClickedorPressedF), ffbutton);
