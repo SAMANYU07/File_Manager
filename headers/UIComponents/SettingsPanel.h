8d34d0a8fec6b74b985c6c01b52aec38787628af
SAMANYU07 <samanyu1003@gmail.com>
2025-08-17 21:19:58 +0530

    Added apply and close button to SettingsPanel

--- headers/UIComponents/SettingsPanel.h
+++ headers/UIComponents/SettingsPanel.h
@@ -15,17 +15,28 @@ class SettingsPanel
                         gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(customAppCheckBox), true);
         }
 
+        static void closeSettingsPanel(GtkWidget *win, gpointer data)
+        {
+                gtk_main_quit();
+                gtk_widget_destroy((GtkWidget *)data);
+        }
+
         SettingsPanel()
         {
                 win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
                 customAppCheckBox = gtk_toggle_button_new_with_label("Open files in custom media viewers.");
-                gtk_widget_set_name(customAppCheckBox, "GtkToggleButton");
                 grid = gtk_grid_new();
+                closeButton = gtk_button_new_with_label("Close");
+                applyAndCloseButton = gtk_button_new_with_label("Apply & Close");
                 gtk_grid_attach(GTK_GRID(grid), gtk_label_new("File Manager Settings"), 0, 0, 1, 1);
                 gtk_grid_attach(GTK_GRID(grid), customAppCheckBox, 0, 1, 1, 1);
+                gtk_grid_attach(GTK_GRID(grid), applyAndCloseButton, 0, 2, 1, 1);
+                gtk_grid_attach(GTK_GRID(grid), closeButton, 1, 2, 1, 1);
                 g_signal_connect(win, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
+                g_signal_connect(closeButton, "clicked", G_CALLBACK(closeSettingsPanel), win);
                 gtk_container_add(GTK_CONTAINER(win), grid);
                 processConfigValues();
+                gtk_widget_set_name(customAppCheckBox, "GtkToggleButton");
                 gtk_window_set_default_size(GTK_WINDOW(win), 400, 400);
                 gtk_widget_show_all(win);
                 gtk_main();
